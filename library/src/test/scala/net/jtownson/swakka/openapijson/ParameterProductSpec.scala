0e5b0a593216da6a6f40947dac2e4df75a635dc8
Jeremy Townson <jeremy.townson@gmail.com>
2017-12-16 20:36:41 +0000

    Remove pointless runtime compilation check from ParameterProductSpec.

--- library/src/test/scala/net/jtownson/swakka/openapijson/ParameterProductSpec.scala
+++ library/src/test/scala/net/jtownson/swakka/openapijson/ParameterProductSpec.scala
@@ -17,32 +17,32 @@ class ParameterProductSpec extends FlatSpec {
 
     case class EmptyParameters()
 
-    "implicitly[JsonFormat[OpenApi[PathItem[EmptyParameters, () => Route, HNil] :: HNil, Nothing]]]" should compile
+    implicitly[JsonFormat[OpenApi[PathItem[EmptyParameters, () => Route, HNil] :: HNil, Nothing]]]
 
-    "implicitly[JsonFormat[OpenApi[PathItem[HNil, () => Route, HNil] :: HNil, Nothing]]]" should compile
+    implicitly[JsonFormat[OpenApi[PathItem[HNil, () => Route, HNil] :: HNil, Nothing]]]
 
-    "implicitly[JsonFormat[OpenApi[PathItem[Unit, () => Route, HNil] :: HNil, Nothing]]]" should compile
+    implicitly[JsonFormat[OpenApi[PathItem[Unit, () => Route, HNil] :: HNil, Nothing]]]
   }
 
   "single param api" should "work" in {
 
     case class Parameters1(p1: QueryParameter[String])
 
-    "implicitly[JsonFormat[OpenApi[PathItem[Parameters1, String => Route, HNil] :: HNil, Nothing]]]" should compile
+    implicitly[JsonFormat[OpenApi[PathItem[Parameters1, String => Route, HNil] :: HNil, Nothing]]]
 
-    "implicitly[JsonFormat[OpenApi[PathItem[QueryParameter[String] :: HNil, String => Route, HNil] :: HNil, Nothing]]]" should compile
+    implicitly[JsonFormat[OpenApi[PathItem[QueryParameter[String] :: HNil, String => Route, HNil] :: HNil, Nothing]]]
 
-    "implicitly[JsonFormat[OpenApi[PathItem[Tuple1[QueryParameter[String]], (String) => Route, HNil] :: HNil, Nothing]]]" should compile
+    implicitly[JsonFormat[OpenApi[PathItem[Tuple1[QueryParameter[String]], (String) => Route, HNil] :: HNil, Nothing]]]
   }
 
   "two param api" should "work" in {
 
     case class Parameters2(p1: QueryParameter[String], p2: HeaderParameter[Option[Int]])
 
-    "implicitly[JsonFormat[OpenApi[PathItem[Parameters2, (String, Option[Int]) => Route, HNil] :: HNil, Nothing]]]" should compile
+    implicitly[JsonFormat[OpenApi[PathItem[Parameters2, (String, Option[Int]) => Route, HNil] :: HNil, Nothing]]]
 
-    "implicitly[JsonFormat[OpenApi[PathItem[QueryParameter[String] :: HeaderParameter[Option[Int]] :: HNil, (String, Option[Int]) => Route, HNil] :: HNil, Nothing]]]" should compile
+    implicitly[JsonFormat[OpenApi[PathItem[QueryParameter[String] :: HeaderParameter[Option[Int]] :: HNil, (String, Option[Int]) => Route, HNil] :: HNil, Nothing]]]
 
-    "implicitly[JsonFormat[OpenApi[PathItem[(QueryParameter[String], HeaderParameter[Option[Int]]), (String, Option[Int]) => Route, HNil] :: HNil, Nothing]]]" should compile
+    implicitly[JsonFormat[OpenApi[PathItem[(QueryParameter[String], HeaderParameter[Option[Int]]), (String, Option[Int]) => Route, HNil] :: HNil, Nothing]]]
   }
 }
